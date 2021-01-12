4a8107439543417dd1910edf1f1dabbe5f426ab8
李杨 <l15692232526>
2021-01-12 15:33:41 +0800

    第二周作业

--- Homework/22+李杨+天津/week 2/nearest_inter.py
+++ Homework/22+李杨+天津/week 2/nearest_inter.py
@@ -1,30 +1,33 @@
 import cv2
 import numpy as np
 
-def nearest_inter(img,out_dim):
+class n_inter:
+    def nearest_inter(self,img,out_dim):
 
-    src_h,src_w,channel=img.shape
-    new_h,new_w,new_c=out_dim[1],out_dim[0],out_dim[2]
-    sh=new_h/src_h
-    wh=new_w/src_w
+        src_h,src_w,channel=img.shape
+        new_h,new_w,new_c=out_dim[1],out_dim[0],out_dim[2]
+        sh=new_h/src_h
+        wh=new_w/src_w
 
-    print("src_h=",src_h,"\nsrc_w=",src_w)
-    print("new_h=",new_h, "\nnew_w=",new_w)
+        print("src_h=",src_h,"\nsrc_w=",src_w)
+        print("new_h=",new_h, "\nnew_w=",new_w)
 
-    if src_h==new_h and src_w==new_w:
-        return img.copy()
+        if src_h==new_h and src_w==new_w:
+            return img.copy()
 
-    ner_img=np.zeros((new_h,new_w,new_c),dtype=np.uint8)
-    for i in range(new_h):
-        for j in range(new_w):
-            x=int(i/sh)
-            y=int(j/wh)
-            ner_img[i,j]=img[x,y]
-    return ner_img
+        ner_img=np.zeros((new_h,new_w,new_c),dtype=np.uint8)
+
+        for i in range(new_h):
+            for j in range(new_w):
+                x=int(i/sh)
+                y=int(j/wh)
+                ner_img[i,j]=img[x,y]
+        return ner_img
 
 if __name__=='__main__':
     img=cv2.imread("lenna.png")
-    ner_img=nearest_inter(img,(700,700,3))
+    n_img=n_inter()
+    ner_img=n_img.nearest_inter(img,(700,700,3))
     cv2.imshow("img",img)
     cv2.imshow("ner_img",ner_img)
     cv2.waitKey(0)
